535da15f78de4fd6f45c378c10c4c90ad3d449a9
xiuyiqian <qianxiuyi23@gmail.com>
2024-06-20 17:46:27 -0500

    create Mockmvc test cases for post request

--- Inventory_Management/Inventory_Management/src/test/java/com/example/Inventory_Management/InventoryManagementApplicationTests.java
+++ Inventory_Management/Inventory_Management/src/test/java/com/example/Inventory_Management/InventoryManagementApplicationTests.java
@@ -1,13 +1,67 @@
 package com.example.Inventory_Management;
 
+import com.example.Inventory_Management.allModels.Product.ProductRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.testcontainers.shaded.com.google.common.net.MediaType.*;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class InventoryManagementApplicationTests {
+	//start the mongodb container
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	private ResultMatcher status;
+
+	//after starting the mongodb container, it will get the replica set url and add it into spring data uri property
+	//it is dynamically added at the time of creating the test
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+
+		ProductRequest productRequest =  getProductRequest();
+		String jsonPostRequestString = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/v2/product")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(jsonPostRequestString))
+				.andExpect(status().isCreated());
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("PS5")
+				.description("SONY Playstation Product")
+				.price(BigDecimal.valueOf(500))
+				.build();
 	}
 
 }
